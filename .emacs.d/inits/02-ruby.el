1f7c9aa7346e84f1fd7988f17e9ef0dac9466cf9
Hidekazu Kobayashi <kobahide789@gmail.com>
2015-04-07 16:08:37 +0000

    fix ruby-mode indent

---
+++ .emacs.d/inits/02-ruby.el
@@ -0,0 +1,14 @@
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
