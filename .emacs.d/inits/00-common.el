60a7e870a26bc872ea118b7112d83829f16e7ae4
Hidekazu Kobayashi <kobahide789@gmail.com>
2015-04-26 17:04:10 +0900

    open in root automagically

--- .emacs.d/inits/00-common.el
+++ .emacs.d/inits/00-common.el
@@ -253,3 +253,28 @@
     (set-char-table-parent table char-width-table)
     (setq char-width-table table)))
 (set-east-asian-ambiguous-width 1)
+
+;; -sudo
+(defun th-rename-tramp-buffer ()
+  (when (file-remote-p (buffer-file-name))
+    (rename-buffer
+     (format "%s:%s"
+             (file-remote-p (buffer-file-name) 'method)
+             (buffer-name)))))
+
+(add-hook 'find-file-hook
+          'th-rename-tramp-buffer)
+
+(defadvice find-file (around th-find-file activate)
+  "Open FILENAME using tramp's sudo method if it's read-only."
+  (if (and (not (file-writable-p (ad-get-arg 0)))
+           (y-or-n-p (concat "File "
+                             (ad-get-arg 0)
+                             " is read-only.  Open it as root? ")))
+      (th-find-file-sudo (ad-get-arg 0))
+    ad-do-it))
+
+(defun th-find-file-sudo (file)
+  "Opens FILE with root privileges."
+  (interactive "F")
+    (set-buffer (find-file (concat "/sudo::" file))))
