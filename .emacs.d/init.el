a2e99d794ba5af7cf5898490ccd4330fbb9fb504
Hidekazu Kobayashi <kobahide789@gmail.com>
2023-06-11 08:31:41 +0000

    Set user-init-file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
